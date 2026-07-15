84f61806fdb680c247aa9bd05487179406435251
abdulrahmaninfra <abdelrahmaninfra@proton.me>
2026-07-15 22:21:22 +0300

    Add main entry point to run windshield application

---
+++ main.py
@@ -0,0 +1,6 @@
+# from src.database.create
+# from src.database.delete import delete_windshield
+# from src.database.update import update_windshield
+from src.database.read import GetWindshield
+
+GetWindshield().print_all_windshield()
